9335f0c803fd5e97f2b369e93d89aeac561ea48d
Sergio Herrando Cuenca <serhercu@upv.edu.es>
2021-05-24 18:27:14 +0200

    changes

--- travello/models.py
+++ travello/models.py
@@ -2,9 +2,48 @@ from django.db import models
 
 # Create your models here.
 
+class Director(models.Model):
+    class Meta:
+        verbose_name_plural = 'director'
+
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return super().__str__()
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     img = models.ImageField(upload_to='pics')
     desc = models.TextField()
     year = models.IntegerField()
     new = models.BooleanField(default=False)
+
+from django.db import models
+
+# Create your models here.
+
+class Director(models.Model):
+    class Meta:
+        verbose_name_plural = 'directors'
+
+    name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return super().__str__()
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    year = models.IntegerField()
+
+    class Genre(models.TextChoices):
+            ACTION ='Action'
+            CARTOON = 'Cartoon'
+            HUMOR = 'Humor'
+            THRILLER = 'Thriller'
+
+    genre = models.TextField(choices=Genre.choices, default=0)
+
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, unique=True)
+
--- travello/views.py
+++ travello/views.py
@@ -8,3 +8,19 @@ def index(request):
     dests = Movie.objects.all()
 
     return render(request, 'index.html', {'dests': dests})
+
+def search(request):
+    title1 = request.GET['title']
+    year1 = request.GET['year']
+
+    movies = Movie.objects.all()
+    searched = []
+
+    for movie in movies:
+        if title1 in movie.title:
+            searched.append(movie) 
+        elif year1 is not '':
+            if int(year1) == movie.year:
+                searched.append(movie)
+    
+    return render(request,'search.html', {'searched': searched})
